e27c3993b084d5d81eed7189ff9f4115d2a1e96f
Hugo@M1Air <44889113+otaku312@users.noreply.github.com>
2021-12-30 17:28:00 +0800

    feat: init starter project

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <template>
-  <v-app id="app">
-    <v-main>
+  <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-    <AtgFormView :form-schema="schema" />
-    </v-main>
-  </v-app>
+  </div>
 </template>
 
 <script lang="ts">
@@ -15,25 +12,6 @@ import HelloWorld from './components/HelloWorld.vue'
 @Component({
   components: {
     HelloWorld,
-  },
-  computed: {
-    schema() {
-      return {
-        title: 'Test Submit with version',
-        subtitle: 'psclinic-csx-dev',
-        tenant: 'psclinic-csx-dev',
-        qrCode: false,
-        preview: false,
-        groups: [{
-          key: 2801884852,
-          legend: 'Group Name',
-          fields: [{
-            inputType: 'input', id: 2045305699, model: 'input_2045305699', label: 'ver', placeholder: '', defaultValue: '', value: '', style: 'default', disabled: false, readonly: false, hidden: false, required: false, render: true, rules: [], ruleCondition: 'or', width: '100%',
-          }],
-        }],
-        mode: 'step',
-      }
-    },
   },
 })
 export default class App extends Vue {}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
 import 'regenerator-runtime/runtime'
 
 import Vue from 'vue'
-import AtgFormBuilder from '@atg/vue-form-builder'
 import isInIcestark from '@ice/stark-app/lib/isInIcestark'
 import setLibraryName from '@ice/stark-app/lib/setLibraryName'
 import App from './App.vue'
 import i18n from './i18n'
-import vuetify from './vuetify'
 
 import { name } from '../package.json'
 
 setLibraryName(name)
 
-Vue.use(AtgFormBuilder)
-
 Vue.config.productionTip = false
 
 function createVue() {
   return new Vue({
-    vuetify,
     i18n,
     render: (h) => h(App),
   })
